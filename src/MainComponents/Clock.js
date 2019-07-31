32c764ae906401732dd3c5e783948389d21b35e8
Dinerville <nadima8@gmail.com>
2019-07-31 20:17:33 +0300

    add clock show/hide

---
+++ src/MainComponents/Clock.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+export default function Clock(props) {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+  });
+  const clock = (
+    <p>
+      {currentTime.getHours()}:
+      {currentTime.getMinutes() < 10
+        ? "0" + currentTime.getMinutes()
+        : currentTime.getMinutes()}
+      :
+      {currentTime.getSeconds() < 10
+        ? "0" + currentTime.getSeconds()
+        : currentTime.getSeconds()}
+    </p>
+  );
+
+  let clockPresense = "";
+  if (props.isDisplayed) {
+    clockPresense = clock;
+  }
+
+  return <div>{clockPresense}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,54 +1,22 @@
 import ReactDOM from "react-dom";
 import React, { useState, useEffect } from "react";
+import MainCanvas from "./MainComponents/MainCanvas";
+import Clock from "./MainComponents/Clock";
 
 import "./styles.css";
 
 function App() {
+  const [isClockDisplayed, setClockDisplay] = useState(true);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Example />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  );
-}
-
-function Clock() {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  useEffect(() => {
-    setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-  });
-
-  return (
-    <div>
-      <p>
-        {currentTime.getHours()}:
-        {currentTime.getMinutes() < 10
-          ? "0" + currentTime.getMinutes()
-          : currentTime.getMinutes()}
-        :
-        {currentTime.getSeconds() < 10
-          ? "0" + currentTime.getSeconds()
-          : currentTime.getSeconds()}
-      </p>
-    </div>
-  );
-}
-
-function Example() {
-  // Объявление переменной состояния, которую мы назовём "count"
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>Вы кликнули {count} раз</p>
-      <button onClick={() => setCount(count + 1)}>Нажми на меня</button>
+      <button onClick={() => setClockDisplay(!isClockDisplayed)}>
+        Показать/спрятать часы
+      </button>
+      <Clock isDisplayed={isClockDisplayed} />
+      <MainCanvas one="first Canvas" />
+      <MainCanvas one="second Canvas" />
     </div>
   );
 }
