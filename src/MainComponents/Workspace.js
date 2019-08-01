e3c2a8804266ae52d94c6142b9e3fda5b8771396
Dinerville <nadima8@gmail.com>
2019-08-01 19:05:27 +0300

    css grid

---
+++ src/MainComponents/Workspace.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+export default function Workspace(props) {
+  let style = {
+    color: "green"
+  };
+  return (
+    <div className="" style={style}>
+      <p>This is workspace</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import React, { useState, useEffect } from "react";
 import MainCanvas from "./MainComponents/MainCanvas";
 import Clock from "./MainComponents/Clock";
+import Workspace from "./MainComponents/Workspace";
 
 import "./styles.css";
 
@@ -14,11 +15,66 @@ function App() {
 
   return (
     <div className="App">
+      <div className="wrapper">
+        <div>
+          <p>
+            lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf asdfa sdf asdf
+            asdf fdgsdfgsdfgsdfgsdfgsdfg sdfg sdfg
+          </p>
+        </div>
+
+        <div className="light">
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf</p>
+        </div>
+
+        <div>
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+        </div>
+
+        <div>
+          <div className="included">
+            <div>
+              <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+            </div>
+            <div>
+              <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+            </div>
+            <div>
+              <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="light">
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf</p>
+        </div>
+
+        <div>
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+        </div>
+
+        <div className="first">
+          <p>
+            lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf asdfa sdf asdf
+            asdf fdgsdfgsdfgsdfgsdfgsdfg sdfg sdfg 333444
+          </p>
+        </div>
+
+        <div className="light second">
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 22222</p>
+        </div>
+
+        <div className="third">
+          <p>lal dfglsdfg lf sldfg lsdfg lsdfgl sldfgl sdflg sdf 33333</p>
+        </div>
+      </div>
+
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={() => setClockDisplay(!isClockDisplayed)} style={styles}>
         Показать/спрятать часы
       </button>
+      <Workspace />
       <Clock isDisplayed={isClockDisplayed} />
       <MainCanvas one="first Canvas" />
       <MainCanvas one="second Canvas" />
