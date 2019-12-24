97b3a9465b1e1b94b74ff0db8251690a637e6b31
Дмитрий Наумов <dzmitrynaumau@outlook.com>
2019-12-24 23:44:43 +0300

    add

--- src/MainComponents/MainCanvas.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-export default function MainCanvas(props) {
-  return (
-    <div>
-      <p>Here will be main canvas {props.one}</p>
-    </div>
-  );
-}
